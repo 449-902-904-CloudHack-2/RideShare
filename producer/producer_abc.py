4eb780f16a0cb4981985766e0221bcd5dcdea55f
Jacob3075 <jacob.bosco3075@gmail.com>
2022-03-29 08:58:48 +0530

    Updated rqbbitmq host ip.

--- producer/producer_abc.py
+++ producer/producer_abc.py
@@ -10,21 +10,21 @@ def index():
 
 
 @app.route('/new_ride')
-def add(cmd):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+def add():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.21.0.3"))
     channel = connection.channel()
     channel.queue_declare(queue="task_queue", durable=True)
     channel.basic_publish(
         exchange="",
         routing_key="task_queue",
-        body=cmd,
+        body="abcd",
         properties=pika.BasicProperties(
             delivery_mode=2,  # make message persistent
         )
     )
     connection.close()
-    return " [x] Sent: %s" % cmd
+    return " [x] Sent: "
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="localhost")
