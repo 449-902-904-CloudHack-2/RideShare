56f582e76a0e8aa9ed416fbc4b04d19a038d8f65
Jacob3075 <jacob.bosco3075@gmail.com>
2022-03-29 10:38:38 +0530

    Fixed flask files.

--- consumer/database_inserting/database_inserting.py
+++ consumer/database_inserting/database_inserting.py
@@ -1,23 +1,26 @@
-import pika
 import json
-import psycopg2
 from datetime import datetime
+
+import pika
+import psycopg2
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
 channel = connection.channel()
+
 
 def callback(ch, method, properties, body):
     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' Processing...')
 
     data = json.loads(body)
 
-    #establishing the connection
+    # establishing the connection
     conn = psycopg2.connect(
-       database="rideshare_db", user='postgres', password='postgres', host='postgres_container', port= '5432'
+        database="rideshare_db", user='postgres', password='postgres', host='postgres_container',
+        port='5432'
     )
     cursor = conn.cursor()
 
-    #query = "INSERT INTO books JSON '" + json.dumps(data) + "'"
+    # query = "INSERT INTO books JSON '" + json.dumps(data) + "'"
 
     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' Processed !')
 
@@ -26,44 +29,3 @@ channel.basic_consume(queue='task_queue', on_message_callback=callback, auto_ack
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
-
-
-
-
-
-'''import time
-
-import pika
-
-sleepTime = 10
-print(' [*] Sleeping for ', sleepTime, ' seconds.')
-time.sleep(30)
-
-print(' [*] Connecting to server ...')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel()
-channel.queue_declare(queue='task_queue', durable=True)
-
-print(' [*] Waiting for messages.')
-
-
-def callback(ch, method, properties, body):
-    print(" [x] Received %s" % body)
-    cmd = body.decode()
-
-    if cmd == 'hey':
-        print("hey there")
-    elif cmd == 'hello':
-        print("well hello there")
-    else:
-        print("sorry i did not understand ", body)
-
-    print(" [x] Done")
-
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
-channel.start_consuming()
-'''
--- producer/producer_abc.py
+++ producer/producer_abc.py
@@ -1,5 +1,7 @@
+import json
+
 import pika
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,15 +11,14 @@ def index():
     return 'OK'
 
 
-@app.route('/new_ride')
+@app.route('/new_ride', methods=["POST"])
 def add():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.21.0.3"))
-    channel = connection.channel()
-    channel.queue_declare(queue="task_queue", durable=True)
+    request_data = request.get_json()
+    channel, connection = get_rabbitmq_connection()
     channel.basic_publish(
         exchange="",
         routing_key="task_queue",
-        body="New Body",
+        body=json.dumps(request_data),
         properties=pika.BasicProperties(
             delivery_mode=2,  # make message persistent
         )
@@ -26,5 +27,12 @@ def add():
     return " [x] Sent: "
 
 
+def get_rabbitmq_connection():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq_container"))
+    channel = connection.channel()
+    channel.queue_declare(queue="task_queue", durable=True)
+    return channel, connection
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost")
+    app.run(debug=True, host="0.0.0.0", port=5000)
