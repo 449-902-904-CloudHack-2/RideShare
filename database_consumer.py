385fbff23dd0a85f8f78a0dde445654418d94e0c
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-03 14:24:23 +0530

    Updated connections in database_consumer.py file

--- database_consumer.py
+++ database_consumer.py
@@ -1,36 +1,37 @@
-# Use this file to setup the database consumer that stores the ride information in the database
 import json
 import time
-
 import pika
 import pymongo
 
 time.sleep(10)
 
-client = pymongo.MongoClient("mongodb://mongodb:27017")
-db = client["ride_matching"]
-collection = db["ride_details"]
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="rabbitmq")
-)
-channel = connection.channel()
+def dbConnection():
+    client = pymongo.MongoClient("mongodb://mongodb_container:27017")
+    db = client["ride_matching_db"]
+    _collection = db["ride_details"]
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbit_mq")
+    )
+    _channel = connection.channel()
+    _channel.queue_declare(queue="database", durable=True)
+    print("Database consumer started...")
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue="database", on_message_callback=callback)
+    channel.start_consuming()
 
-channel.queue_declare(queue="database", durable=True)
-print("Database consumer started...", flush=True)
+    return _collection, _channel
+
+
+collection, channel = dbConnection()
 
 
 def callback(ch, method, properties, body):
     body = json.loads(body)
     body["_id"] = properties.message_id
-    print(f"Consumed {body} from database queue", flush=True)
+    print(f"Consumed {body} from database queue")
 
     collection.insert_one(body)
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue="database", on_message_callback=callback)
-
-channel.start_consuming()
