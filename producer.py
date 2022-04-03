a03fe65f20c8347c3b15e30323bf3f3e72e52d05
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-03 14:35:41 +0530

    Updated connections in producer.py file

--- producer.py
+++ producer.py
@@ -1,7 +1,5 @@
-# Code for Producer
 import json
 from uuid import uuid4
-
 import pika
 from flask import Flask, request
 
@@ -9,44 +7,41 @@ consumers = []
 
 app = Flask(__name__)
 
+def createConnections():
+    _connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbit_mq")
+    )
+    # Create channel to publish to ride_match queue
+    _channel = _connection.channel()
+    _channel.queue_declare(queue="ride_match", durable=True)
+    _channel.queue_declare(queue="database", durable=True)
+
+    return _connection, _channel
+
+def sendDataToQueue(queue, data, task_id, channel):
+    channel.basic_publish(
+        exchange="",
+        routing_key=queue,
+        body=data,
+        properties=pika.BasicProperties(
+            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,
+            message_id=task_id
+        )
+    )
+
+
 
 @app.route("/new_ride", methods=["POST"])
 def new_ride():
+    connection, channel=createConnections()
     body = json.dumps(request.form)
     task_id = "task_" + uuid4().hex
-
-    # Connect to rabbitmq over tcp
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="rabbitmq")
-    )
-
-    # Create channel to publish to ride_match queue
-    channel = connection.channel()
-    channel.queue_declare(queue="ride_match", durable=True)
-    channel.basic_publish(
-        exchange="",
-        routing_key="ride_match",
-        body=body,
-        properties=pika.BasicProperties(
-            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,
-            message_id=task_id
-        )
-    )
-
+    sendDataToQueue("ride_matching", body, task_id, channel)
     print(f"Received task {body} via POST, published to queue", flush=True)
 
     # Create new channel to publish to database queue
-    channel = connection.channel()
-    channel.queue_declare(queue="database", durable=True)
-    channel.basic_publish(
-        exchange="",
-        routing_key="database",
-        body=body,
-        properties=pika.BasicProperties(
-            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,
-            message_id=task_id
-        )
-    )
+
+    sendDataToQueue("database", body, task_id, channel)
 
     connection.close()
 
